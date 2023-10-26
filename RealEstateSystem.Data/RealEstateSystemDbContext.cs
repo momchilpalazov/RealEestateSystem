2f97851f0f48701de6bbd5238f3cb32691b5cc0a
Momchil Palazov <momchil.palazov@gmail.com>
2023-10-26 17:00:11 +0200

    Create First Base Structure

---
+++ RealEstateSystem.Data/RealEstateSystemDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateSystem.Data
+{
+    public class RealEstateSystemDbContext:DbContext
+    {
+
+        public RealEstateSystemDbContext(DbContextOptions<RealEstateSystemDbContext> options)
+            : base(options)
+        {
+
+        }
+
+
+    }
+}
