4ae833798eaa0815fb1d858f29e0ea30dddddadf
Momchil Palazov <momchil.palazov@gmail.com>
2023-10-29 17:14:55 +0100

    Add Migrations

--- RealEstateSystem.Data/Configurations/CategoryEntityConfigurations.cs
+++ RealEstateSystem.Data/Configurations/CategoryEntityConfigurations.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstateSystem.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,48 @@ using System.Threading.Tasks;
 
 namespace RealEstateSystem.Data.Configurations
 {
-    internal class CategoryEntityConfigurations
+    public class CategoryEntityConfigurations : IEntityTypeConfiguration<Category>
     {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(GetCategories());   
+        }
+
+
+        private Category[] GetCategories()
+        {
+
+            ICollection<Category> categories = new List<Category>();
+
+            Category category;
+
+
+            category = new Category
+            {
+                Id = 1,
+                Name = "Cottage"
+
+            };
+            categories.Add(category);
+
+            category=new Category
+            {
+                Id = 2,
+                Name = "Single-Family"
+
+            };
+            categories.Add(category);
+
+            category = new Category
+            {
+                Id = 3,
+                Name = "Duplex"
+
+            };
+            categories.Add(category);
+
+           return categories.ToArray();
+        }
+
     }
 }
