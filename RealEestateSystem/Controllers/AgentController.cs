01a51ca279ab3d5f05236219d2d7277a34a7d44a
Momchil Palazov <momchil.palazov@gmail.com>
2023-10-30 17:34:36 +0100

    Creating first structure the Agent Controller Class,Views and ViewModels

---
+++ RealEestateSystem/Controllers/AgentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateSystem.Controllers
+{
+    public class AgentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
