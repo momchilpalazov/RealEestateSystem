d5b02466287e4d800f98b358c41aea149ea6e504
Momchil Palazov <momchil.palazov@gmail.com>
2023-11-02 10:09:53 +0100

    Cretae Agent ViewModel

--- RealEstateSystem.Web.DataView/Agents/BecomeAgentFormModel.cs
+++ RealEstateSystem.Web.DataView/Agents/BecomeAgentFormModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace RealEstateSystem.Models.ViewModels.Agents
 {
     public class BecomeAgentFormModel
     {
-
-
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        [StringLength(10, MinimumLength = 15, ErrorMessage = "The phone number must be 10 digits long.")]
+        public string PhoneNumber { get; set; }=null!;
 
 
     }
