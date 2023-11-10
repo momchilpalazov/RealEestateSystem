e93e70836fe10a7e15db8e65d93da86bde8f87e4
Momchil Palazov <momchil.palazov@gmail.com>
2023-11-10 11:23:49 +0100

    Create House Form Model View

---
+++ RealEstateSystem.Common/ViewModelValidationConstants.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace RealEstateSystem.Common
+{
+    public static class ViewModelValidationConstants
+    {
+
+        public static class HouseFormModel
+        {
+
+            public const int TitleMinLength = 10;
+            public const int TitleMaxLength = 50;
+
+            public const int AddressMinLength = 30;
+            public const int AddressMaxLength = 150;
+
+            public const int DescriptionMinLength = 50;
+            public const int DescriptionMaxLength = 500;
+
+            public const string PricePerMonthMinLength = "0";
+            public const string PricePerMonthMaxLength = "2000";
+        
+        
+        
+        
+        }
+
+
+
+    }
+}
--- RealEstateSystem.Web.DataView/House/HouseFormModel.cs
+++ RealEstateSystem.Web.DataView/House/HouseFormModel.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RealEstateSystem.Models.ViewModels.Category;
+using static RealEstateSystem.Common.ViewModelValidationConstants.HouseFormModel;
+using System.ComponentModel.DataAnnotations;
+
+
 
 namespace RealEstateSystem.Models.ViewModels.House
 {
     public class HouseFormModel
     {
+
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Price Per Month")]
+        [Range(typeof(decimal), PricePerMonthMinLength, PricePerMonthMaxLength,ErrorMessage ="Prise per Month must be positive number and less than {2} Euro")]
+        public decimal PricePerMonth { get; set; }
+
+        [Display(Name = "Category")]    
+        public int CategoryId { get; set; }
+
+
+        public ICollection<CategoryHouseServiceViewModel> Categories { get; set; } = new HashSet<CategoryHouseServiceViewModel>();
+
+
+
+
     }
 }
