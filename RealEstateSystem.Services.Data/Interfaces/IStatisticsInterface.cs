299fac5218b13ffef02e8d4603e63c48a0d3e547
Momchil Palazov <momchil.palazov@gmail.com>
2023-12-16 17:30:25 +0100

    Create Statistics Web API IstatisticsInterface StatisticService Method:GetStatistcs()

---
+++ RealEstateSystem.Services.Data/Interfaces/IStatisticsInterface.cs
@@ -0,0 +1,15 @@
+﻿using RealEstateSystem.Models.ViewModels.Stastic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateSystem.Services.Data.Interfaces
+{
+    public interface IStatisticsInterface
+    {
+        StatisticServiceModelViewModel GetStatistics();
+
+    }
+}
---
+++ RealEstateSystem.Services.Data/StatisticsService.cs
@@ -0,0 +1,38 @@
+﻿using RealEstateSystem.Data;
+using RealEstateSystem.Models.ViewModels.Stastic;
+using RealEstateSystem.Services.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateSystem.Services.Data
+{
+    public class StatisticsService : IStatisticsInterface
+    {
+        private readonly RealEstateSystemDbContext dbContext;
+
+        public StatisticsService(RealEstateSystemDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        public StatisticServiceModelViewModel GetStatistics()
+        {
+
+            var totalHouses = this.dbContext.Hauses.Count();
+            var totalRents = this.dbContext.Hauses.Where(t=>t.RenterId!=null).Count();
+
+            return new StatisticServiceModelViewModel
+            {
+                TotalHouses= totalHouses,
+                TotalRents = totalRents
+
+            };
+
+           
+        }
+    }
+}
