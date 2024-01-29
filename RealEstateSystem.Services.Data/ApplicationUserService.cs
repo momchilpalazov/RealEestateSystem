cf71974437c8d5eae5eecb16d48cb19fe1be80ff
Momchil Palazov <momchil.palazov@gmail.com>
2024-01-29 12:09:20 +0100

    Add Role for Admin

--- RealEstateSystem.Services.Data/ApplicationUserService.cs
+++ RealEstateSystem.Services.Data/ApplicationUserService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace RealEstateSystem.Services.Data
 {
     public class ApplicationUserService : IApplicationUserInterface
---
+++ RealEstateSystems.Web.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using RealEstateSystem.Data.Models;
+using static RealEstateSystem.Common.AdminRoleConstant;
+
+namespace RealEstateSystems.Web.Infrastructure.Extensions
+{
+    public  class ApplicationBuilderExtensions
+    {
+
+        public static IApplicationBuilder SeedAdmin(IApplicationBuilder application,string email)
+        {
+
+
+            using var serviceScope = application.ApplicationServices.CreateScope();
+
+            var serviceProvider = serviceScope.ServiceProvider;
+
+            var userManager=serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var roleManger=serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+            Task.Run(async () =>
+            {
+                if (await roleManger.RoleExistsAsync(AdminRoleName)) 
+                { 
+                    return;                
+                
+                }
+
+                IdentityRole<Guid> role= new IdentityRole<Guid>(AdminRoleName);
+
+                await roleManger.CreateAsync(role);
+
+                ApplicationUser adminUser = await userManager.FindByEmailAsync(email);
+
+                await userManager.AddToRoleAsync(adminUser, AdminRoleName);
+
+            }).GetAwaiter().GetResult();
+
+            return application;         
+
+
+            
+        }
+
+
+
+
+    }
+}
